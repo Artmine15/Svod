b7121d12d401d0f413076dba2c54623609a6bde6
Artem <artsel256@gmail.com>
2025-10-04 17:05:48 +0700

    Added navigation keys

--- app/src/main/java/com/artmine15/svod/MainActivity.kt
+++ app/src/main/java/com/artmine15/svod/MainActivity.kt
@@ -6,23 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
-import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import com.artmine15.svod.ui.screens.AuthenticationScreen
 import com.artmine15.svod.ui.theme.SvodTheme
 import dagger.hilt.android.AndroidEntryPoint
-
-object AuthenticationScreenKey : NavKey
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -33,7 +26,6 @@ class MainActivity : ComponentActivity() {
             SvodTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val backStack = remember { mutableStateListOf<NavKey>(AuthenticationScreenKey) }
-                    //Text(text = "", modifier = Modifier.padding(innerPadding))
                     NavDisplay(
                         modifier = Modifier.padding(innerPadding),
                         backStack = backStack,
---
+++ app/src/main/java/com/artmine15/svod/NavigationKeys.kt
@@ -0,0 +1,5 @@
+package com.artmine15.svod
+
+import androidx.navigation3.runtime.NavKey
+
+object AuthenticationScreenKey : NavKey
